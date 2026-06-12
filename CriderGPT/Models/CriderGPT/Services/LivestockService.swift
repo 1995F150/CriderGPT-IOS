8ba767233bfedd6d9207ec50afa77070280b443b
Jessie Crider (CriderGPT) <jessiecrider3@gmail.com>
2026-06-12 18:50:24 -0400

    Delete CriderGPT/Models/CriderGPT/Services/LivestockService.swift

--- CriderGPT/Models/CriderGPT/Services/LivestockService.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-import Supabase
-class LivestockService {
-    private let supabase = SupabaseService.shared.client
-    func fetchAnimals() async throws -> [LivestockAnimal] {
-        return try await supabase.from("livestock_animals").select("*").execute().value
-    }
-}
